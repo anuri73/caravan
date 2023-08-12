a883513d65e4269b132cd95c586d4425014c12f2
19842639 <urzhenaliev@sberbank.ru>
2023-08-12 20:47:04 +0300

    correct postgresql version

--- project/Version.scala
+++ project/Version.scala
@@ -1,7 +1,7 @@
 object Version {
   val Flyway          = "9.16.3"
   val LogbackClassic  = "1.4.7"
-  val Postgresql      = "40.6.0"
+  val Postgresql      = "42.6.0"
   val Slick           = "3.4.1"
   val TestContainers  = "1.0.0-alpha1"
   val Zio             = "2.0.15"
